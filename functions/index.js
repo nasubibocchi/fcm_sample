f0c0681c76c3ddeda8415e1e59a18e5e78f5af53
nasubibocchi <matsuhito125@gmail.com>
2021-11-04 16:32:10 +0900

    deploy

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,35 @@
 const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+admin.initializeApp();
+const firestore = admin.firestore()
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+//push通知実行メソッド
+const pushMessage = (fcmToken, text) => ({
+    notification: {
+        title: '新しいメッセージがあります',
+        body:  `${text}`,
+    },
+    apns: {
+        headers: {
+            'apns-priority': '10'
+        },
+        payload: {
+            aps: {
+                badge: 9999,
+                sound: 'default'
+            }
+        }
+    },
+    data: {
+        data: 'test',
+    },
+    token: fcmToken
+  });
+
+
+exports.sendMessage = functions.firestore.document('users/{fcmToken}/post/{postId}').onCreate(async (snapshot, context) => {
+    const fcmToken = snapshot.data()['fcmToken'];
+    const text = snapshot.data()['text'];
+    try {pushMessage(fcmToken, text);} catch (e) {console.log(e);}
+});
